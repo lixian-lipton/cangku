58eb9dcbe8fc3415f1ded8cb3e5b7e681c050235
lixian-lipton <2480050860@qq.com>
2023-09-02 23:37:38 +0800

    level 1-1

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int ans1,ans2;
 int main()
 {
     scanf("%d %d %d",&a,&b,&c);
-    ans1=fabs(a-c);
-    ans2=ans1+fabs(a-b);
+    ans1=abs(a-c);
+    ans2=ans1+abs(a-b);
     printf("%d %d %d\n%d %d %d\n%d %d %d\n",c,0,0,a,ans1,1,b,ans2,0);
     return 0;
 }
