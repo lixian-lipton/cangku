fc2c16c8ba0e3b7f478db107d8719f7967e4f249
lixian-lipton <2480050860@qq.com>
2023-09-05 23:51:27 +0800

    level1

--- 1-3/main.c
+++ 1-3/main.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 bool print;//打印判断
 int run=1,human[11][2],level,time,load=0,flag;//human[i][1]储存第i层客户目的地,human[i][0]储存乘客状态
-int c_in;                                //乘客状态：0为下电梯或不存在，1为等待电梯，2为在电梯上
+int c_in1,c_in2;                                //乘客状态：0为下电梯或不存在，1为等待电梯，2为在电梯上
 bool empty()//判断是否完成所有任务
 {
     for(int i=1;i<=10;i++)
@@ -14,25 +14,19 @@ void out_status()
 {
     printf("%d %d %d\n",level,time,load);
 }
-int cin_request(int a)
-{
-    int b;
-    printf("Passenger from the %d floor, please enter your motivation below.\n",a);
-    scanf(("%d"),&b);
-    return b;
-}
 int main()
 {
-    printf("Please enter one floor with passenger waiting on per line, and enter a '0' at the end.\n");
+    printf("Please enter passengers' information (one person per line), and enter a '0 0' at the end.\n");
     for(int i=0;i<=1;i++)
         for(int j=1;j<=10;j++)
             human[j][i]=0;
     while(1)
     {
-        scanf("%d",&c_in);
-        if(c_in==0)
+        scanf("%d %d",&c_in1,&c_in2);
+        if(c_in1==0)
             break;
-        human[c_in][0]=1;
+        human[c_in1][0]=1;
+        human[c_in1][1]=c_in2;
     }
     printf("1,0,0\n");//打印初始状态
     time=-1;//后面循环中每层判定都会time++，但出发的那层判定不应当计入总时间
@@ -55,7 +49,6 @@ int main()
         if(human[level][0]==1&&load<4)//后上
         {
             print=1;
-            human[level][1]=cin_request(level);
             human[level][0]=2;
             load++;
         }
